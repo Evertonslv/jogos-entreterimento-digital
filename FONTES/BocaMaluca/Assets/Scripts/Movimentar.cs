6cfd30ce5c1077b27f5360d06dafbab02f7417e7
Altequer <jonathan.michels7@gmail.com>
2020-04-24 02:06:26 -0300

    Criando fase do jogo

---
+++ FONTES/BocaMaluca/Assets/Scripts/Movimentar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimentar : MonoBehaviour
+{
+	private Material objMovimentar;
+	public float velocidade;
+	private float deslocamento;
+	
+	void Start()
+	{
+		objMovimentar = GetComponent<Renderer>().material;
+	}
+
+	void Update()
+	{
+		deslocamento += 0.001f;
+		objMovimentar.SetTextureOffset("_MainTex", new Vector2(0, deslocamento * velocidade));
+	}
+}
