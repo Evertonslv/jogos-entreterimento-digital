d854bb0d73f085673b47e2aa4203ebccb0ca498e
Everton <evertondasilva1@live.com>
2020-05-01 03:07:29 -0300

    adicionado botao inicial e contagem regressiva para iniciar o jogo

--- FONTES/BocaMaluca/Assets/Scripts/Movimentar.cs
+++ FONTES/BocaMaluca/Assets/Scripts/Movimentar.cs
@@ -4,17 +4,30 @@ using System.Collections;
 public class Movimentar : MonoBehaviour
 {
 	private Material objMovimentar;
+	public Animator animator;
+	public SpriteRenderer spriteRenderer;
 	public float velocidade;
 	private float deslocamento;
-	
+	private float contagem = 5f;
+
 	void Start()
 	{
 		objMovimentar = GetComponent<Renderer>().material;
+		animator.speed = 0.1f;
 	}
 
 	void Update()
 	{
-		deslocamento += 0.001f;
-		objMovimentar.SetTextureOffset("_MainTex", new Vector2(0, deslocamento * velocidade));
+		if(contagem > 0) {
+			contagem -= Time.deltaTime;
+		} else {
+			spriteRenderer.enabled = false;
+			deslocamento += 0.001f;
+			objMovimentar.SetTextureOffset("_MainTex", new Vector2(0, deslocamento * velocidade));
+		}
+	}
+
+	void OnGUI() {
+
 	}
 }
